b0f186d962307a2d113eea7622ecb0ddc219ab57
Shanu Dey <sd4shanudey@gmail.com>
2022-03-14 10:54:54 +0530

    close browser after video ends
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- app.js
+++ app.js
@@ -12,13 +12,19 @@ const browser = await puppeteer.launch({
 try {
   const [page] = await browser.pages();
 
-  await page.goto('https://www.youtube.com/watch?v=-kjaV5fV9iw');
+  await page.goto('https://www.youtube.com/watch?v=dn69J4ourP8');
 
   await page.waitForSelector('.ytp-fullscreen-button.ytp-button');
 
   await page.evaluate(() => {
     document.querySelector('.ytp-fullscreen-button.ytp-button').click();
   });
+
+  // close video player after video ends
+  await page.waitForFunction(
+    "document.querySelector('.ytp-time-current').innerHTML === document.querySelector('.ytp-time-duration').innerHTML"
+  );
+  await browser.close();
 } catch (err) {
   console.error(err);
 }
