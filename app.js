aa9f1c5313d519da77056d54273ededce1ba816e
Shanu Dey <sd4shanudey@gmail.com>
2022-03-14 17:09:54 +0530

    integreate video player with express app
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import VideoPlayer from './main/VideoPlayer.js';
+
 const app = express();
+const videoPlayer = new VideoPlayer();
 
 // Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static('public'));
-app.get('/index.html', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/' + 'index.html');
 });
 
 app.post('/start_video', urlencodedParser, function (req, res) {
-  console.log(req.body);
-  // Prepare output in JSON format
-  let response = {
-    video_url: req.body.video_url,
-  };
-  console.log(response);
-  res.end(JSON.stringify(response));
-});
-
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-  res.send('hello world');
+  const video_url = req.body.video_url;
+  // console.log(video_url);
+  videoPlayer.playVideo(video_url);
+  res.redirect('/');
 });
 
 app.listen(4444);
